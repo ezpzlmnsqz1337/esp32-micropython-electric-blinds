f3fb934d8395575f4b26795d5236e0e8277e2764
Zdeněk Braun <zdenek.braun@siemens.com>
2020-09-09 10:46:49 +0200

    Password loading from file

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from microWebSrv import MicroWebSrv
 import machine
 import stepper
 
-USER_PASSWORD = "3c0583dde3ee32103f2ef98ab78eb831c3d600931cbf70b8ee7fe1a85685dcb5"
+USER_PASSWORD = ''
+with open('welcome.txt', 'r') as f:
+    USER_PASSWORD = f.readline()
 
 motors = [
     stepper.MyStepper(5, 18, 19, 21, 0.001),
@@ -28,7 +30,7 @@ timMotor0.init(period=1, mode=machine.Timer.PERIODIC,
 
 
 def _acceptWebSocketCallback(webSocket, httpClient):
-    print("WS ACCEPT")
+    print('WS ACCEPT')
     webSocket.RecvTextCallback = _recvTextCallback
     webSocket.RecvBinaryCallback = _recvBinaryCallback
     webSocket.ClosedCallback = _closedCallback
@@ -40,32 +42,32 @@ def _recvTextCallback(webSocket, msg):
         steps = int(msg.split(':')[2])
         motor = motors[index]
         motor.setTargetPosition(motor.getPosition() + steps)
-        webSocket.SendText("motor:%i:goto:%s " %
+        webSocket.SendText('motor:%i:goto:%s ' %
                            (index, motor.getPosition() + steps))
     elif 'down' in msg:
         index = int(msg.split(':')[1])
         steps = int(msg.split(':')[2])
         motor = motors[index]
         motor.setTargetPosition(motor.getPosition() - steps)
-        webSocket.SendText("motor:%i:go to: %i" %
+        webSocket.SendText('motor:%i:go to: %i' %
                            (index, motor.getPosition() - steps))
     elif 'stop' in msg:
         index = int(msg.split(':')[1])
         motor = motors[index]
         motor.setTargetPosition(motor.getPosition())
         motor.disable()
-        webSocket.SendText("motor:%i, stop" % index)
+        webSocket.SendText('motor:%i, stop' % index)
     elif 'close' in msg:
         index = int(msg.split(':')[1])
         motor = motors[index]
         motor.setTargetPosition(motor.getLimit())
-        webSocket.SendText("motor: %i, close, limit: %i " %
+        webSocket.SendText('motor: %i, close, limit: %i ' %
                            (index, motor.getLimit()))
     elif 'open' in msg:
         index = int(msg.split(':')[1])
         motor = motors[index]
         motor.setTargetPosition(0)
-        webSocket.SendText("motor: %i, open, bottom limit: %i " %
+        webSocket.SendText('motor: %i, open, bottom limit: %i ' %
                            (index, 0))
     elif 'setTopPosition' in msg:
         password = msg.split(':')[2]
@@ -73,7 +75,7 @@ def _recvTextCallback(webSocket, msg):
             index = int(msg.split(':')[1])
             motor = motors[index]
             motor.setTopPosition()
-            webSocket.SendText("setTopPosition:motor:%i:position:%i" %
+            webSocket.SendText('setTopPosition:motor:%i:position:%i' %
                                (index, motor.getPosition()))
     elif 'setLimit' in msg:
         password = msg.split(':')[2]
@@ -81,27 +83,27 @@ def _recvTextCallback(webSocket, msg):
             index = int(msg.split(':')[1])
             motor = motors[index]
             motor.setLimit()
-            webSocket.SendText("setLimit:motor:%i:position:%i" %
+            webSocket.SendText('setLimit:motor:%i:position:%i' %
                                (index, motor.getPosition()))
     elif 'getBlindsPosition' in msg:
-        webSocket.SendText("blindsPosition:motor:%i:position:%i" %
+        webSocket.SendText('blindsPosition:motor:%i:position:%i' %
                            (0, motors[0].getPosition()))
-        webSocket.SendText("blindsPosition:motor:%i:position:%i" %
+        webSocket.SendText('blindsPosition:motor:%i:position:%i' %
                            (1, motors[1].getPosition()))
 
 
 def _recvBinaryCallback(webSocket, data):
-    print("WS RECV DATA : %s" % data)
+    print('WS RECV DATA : %s' % data)
 
 
 def _closedCallback(webSocket):
-    print("WS CLOSED")
+    print('WS CLOSED')
 
 # ----------------------------------------------------------------------------
 
 # routeHandlers = [
-#	( "/test",	"GET",	_httpHandlerTestGet ),
-#	( "/test",	"POST",	_httpHandlerTestPost )
+#	( '/test',	'GET',	_httpHandlerTestGet ),
+#	( '/test',	'POST',	_httpHandlerTestPost )
 # ]
 
 
@@ -109,8 +111,8 @@ srv = MicroWebSrv(webPath='www/')
 srv.MaxWebSocketRecvLen = 256
 srv.WebSocketThreaded = True
 srv.AcceptWebSocketCallback = _acceptWebSocketCallback
-print("server start")
+print('server start')
 srv.Start(threaded=True)
-print("server started")
+print('server started')
 
 # ----------------------------------------------------------------------------
